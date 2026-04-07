244cd2819754cca08dec421f761e904486e95632
fanov <fanovalesya@gmail.com>
2026-04-07 13:30:15 +0700

    add Activi2ty: Fragment + ViewModel

---
+++ main/src/main/java/ui/ShoppingListScreen.kt
@@ -0,0 +1,39 @@
+package ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingListScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Список покупок")
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Экран списка покупок готов к разработке",
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+    }
+}
+
