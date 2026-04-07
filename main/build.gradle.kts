6c80776bb77df0826603d7ba822767322f540373
fanov <fanovalesya@gmail.com>
2026-04-07 14:01:42 +0700

    add Activi2ty: Fragment + ViewModel

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -40,13 +40,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
+    implementation (libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
@@ -56,4 +56,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 }
--- main/src/main/java/ci/nsu/mobile/main/ShoppingListScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/ShoppingListScreen.kt
@@ -1,7 +1,9 @@
 package ci.nsu.mobile.main
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -11,7 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.getValue
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,6 +23,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 fun ShoppingListScreen() {
     val viewModel: ShoppingViewModel = viewModel()
     val uiState by viewModel.uiState.collectAsState()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -33,12 +38,19 @@ fun ShoppingListScreen() {
                 .fillMaxSize()
                 .padding(innerPadding)
                 .padding(16.dp)
-        ) {
+        ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+        {
             Text(
                 text = "Экран списка покупок готов к разработке",
                 modifier = Modifier.padding(bottom = 16.dp)
             )
         }
     }
+}
 }
 
