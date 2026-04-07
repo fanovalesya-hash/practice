53871038b3a98c5a06042e26f25d15cd1c2716e6
fanov <fanovalesya@gmail.com>
2026-04-07 13:39:03 +0700

    add Activi2ty: Fragment + ViewModel

--- main/src/main/java/ci/nsu/mobile/main/ShoppingListScreen.kt
+++ main/src/main/java/ci/nsu/mobile/main/ShoppingListScreen.kt
@@ -8,8 +8,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 
 @OptIn(ExperimentalMaterial3Api::class)
