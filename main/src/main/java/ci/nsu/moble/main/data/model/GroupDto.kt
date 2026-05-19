37d8bbafad934e1b156a3d7a3bc53bc2c7d40e3d
nasenkinaoe <fanovalesya@gmail.com>
2026-05-19 14:26:48 +0700

    в

--- main/src/main/java/ci/nsu/moble/main/data/model/GroupDto.kt
+++ main/src/main/java/ci/nsu/moble/main/data/model/GroupDto.kt
@@ -10,3 +10,43 @@ data class GroupDto(
     @SerialName("groupName")
     val name: String
 )
+
+@Serializable
+data class PersonDto(
+    val firstName: String,
+    val lastName: String,
+    val middleName: String?,
+    val birthDate: String, // Обычно формат "YYYY-MM-DD"
+    val gender: String,
+    val groupId: Int
+)
+
+@Serializable
+data class RegisterRequest(
+    val login: String,
+    val password: String,
+    val email: String,
+    val phoneNumber: String,
+    @SerialName("roleId")
+    val roleId: Int = 1, // Всегда 1, как в задании
+    @SerialName("authAllowed")
+    val authAllowed: Boolean = true,
+    val person: PersonDto
+)
+@Serializable
+data class LoginRequest(
+    val login: String,
+    val password: String
+)
+@Serializable
+data class LoginResponse(
+    @SerialName("token")
+    val token: String
+)
+@Serializable
+data class UserDto(
+    val id: Int,
+    val login: String,
+    val email: String,
+    val person: PersonDto? // Вложенная структура персоны
+)
